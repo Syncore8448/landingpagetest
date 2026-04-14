04830cffebb18ecc3bb33748cf915d7f6b96aa01
Syncore8448 <pawelkowalczyk107@gmail.com>
2026-04-15 00:05:09 +0200

    14.04 v3

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 import styles from "./Navbar.module.css";
+import Image from "next/image";
 
 export default function Navbar() {
   const [scrolled, setScrolled] = useState(false);
@@ -19,9 +20,14 @@ export default function Navbar() {
     <div className={`${styles.wrapper} ${scrolled ? styles.scrolled : ""}`}>
       <div className={styles.navbar}>
         <div className={styles.logo}>
-          <span className={styles.logoIcon}>◎</span>
-          apilo
+           <Image
+              src="/syncorelogo.png"
+              alt="Apilo logo"
+              width={150}
+              height={150}
+           />
         </div>
+          
 
         <div className={styles.links}>
           <a href="#">Product</a>
